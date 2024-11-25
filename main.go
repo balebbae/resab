e576b1d91381c823791cd44dac2e02925866e447
Caleb Bae <baecal000@gmail.com>
2024-11-25 13:43:08 -0600

    make new read

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/balebbae/resaB/jsonmanager"
+
 )
 
 func main() {
@@ -20,15 +21,8 @@ func main() {
 		fmt.Println(err)
 		return
 	}
-
-	// Print the number of employees
-	fmt.Printf("Number of employees: %d\n", len(employees))
-
-	// Print the employees and their shifts
-	for _, employee := range employees {
-		fmt.Printf("Employee Name: %s, Position: %s\n", employee.Name, employee.Position)
-		for _, shift := range employee.Shifts {
-			fmt.Printf("  Day: %s, DayShift: %d, Priority: %d\n", shift.Day, shift.DayShift, shift.Priority)
-		}
+	for _, e := range employees {
+		e.Print()
 	}
+	// Print the number of employees
 }
--- models/employee.go
+++ models/employee.go
@@ -1,5 +1,8 @@
 package models
 
+import "fmt"
+
+
 type Employee struct {
 	Name     string                  
 	Position string
@@ -13,15 +16,8 @@ type Shift struct {
 }
 
 func (e *Employee) Print() {
-	// fmt.Printf("Employee Name: %s\n", e.Name)
-	// monEveningPriority := e.Position["server"].Shifts["mon"].Shift["2"]
-	// fmt.Printf("Employee's priority for Monday evening shift: %s\n", monEveningPriority)
-
-	// // Print all shift priorities for the employee
-	// fmt.Println("\nEmployee's Shift Priorities:")
-	// for day, dayShift := range e.Position["server"].Shifts {
-	// 	for shiftNum, priority := range dayShift.Shift {
-	// 		fmt.Printf("Day: %s, Shift: %s, Priority: %s\n", day, shiftNum, priority)
-	// 	}
-	// }
+	fmt.Printf("Employee Name: %s, Position: %s\n", e.Name, e.Position)
+		for _, shift := range e.Shifts {
+			fmt.Printf("  Day: %s, DayShift: %d, Priority: %d\n", shift.Day, shift.DayShift, shift.Priority)
+		}
 }
